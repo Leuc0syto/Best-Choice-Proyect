0e3fc9fe70f248347377ab4b7e152cc830f8b776
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-01 21:50:33 +0100

    últimos cambios

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\PublicController;
 
 
 
-Route::get('/', [PublicController::class, 'home'])->name('welcome');
+Route::get('/', [PublicController::class, 'home'])->name('home');
 
 
 Route::get('/ads/create', [AdController::class,'create'])->name('ads.create');
