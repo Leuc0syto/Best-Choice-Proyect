d135dbcb9ffdce1429516086ff5e5d690317d9e4
Micky <micpro79@mail.ru>
2023-03-01 20:15:27 +0100

    user2

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ad;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
@@ -11,5 +12,10 @@ class PublicController extends Controller
     public function index(){
         $ads = Ad::orderBy('created_at', 'desc')->take(6)->get();
         return view('welcome',compact('ads'));
+    }
+
+    public function adsByCategory(Category $category) {
+        $ads = $category->ads()->get();
+        return view('ad.by-category', compact('category', 'ads'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-    <x-slot name='title'>BestChoice - Homepage</x-slot>
+    <x-slot name='title'>BestChoice - {{ $category->name }} ads</x-slot>
     <h1>Bienvenido a BestChoice.com</h1>
     
+
+
+
+
 </x-layout>
