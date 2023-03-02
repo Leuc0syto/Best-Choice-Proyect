86e04838540d9bf08457a6b61d30f2d6a53016bb
Leudys Torres <leudystorres30@gmail.com>
2023-03-02 19:09:56 +0100

    user 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
             <div class="row">
                 <div class="col-12">
                     <h1>Bienvenido a BestChoice</h1>
-                    <h3>Anuncios por categoría: </h3> {{-- despues borrar si da error --}}
                 </div>
             </div>
             <div class="row">
@@ -20,7 +19,7 @@
                                 <i>{{$ad->created_at->format('d/m/Y')}}</i>
                             </div>
                             <div class="card-subtitle mb-2">
-                                <small>{{$ad->user->name}}</small>
+                                {{-- <small>{{$ad->user->name}}</small> --}}
                             </div>
                             <a href="{{ route("ads.show", $ad) }}" class="btn btn-primary">Mostrar Más</a>
                         </div>
