c2e044de6be01395b996cb5d52c495a47eb54a90
Leudys Torres <leudystorres30@gmail.com>
2023-03-04 23:50:33 +0100

    nav y hero

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,42 @@
-<nav class="navbar navbar-expand-lg bg-body-primary my-nav">
-    <div class="container-fluid">
-        <a class="navbar-brand text-white"  aria-current="page" href="{{ route('home') }}"><img src="{{-- {{asset('assets/img/logo.jpg')}} --}}" style="width: 120px">BestChoice</a>
+<nav class="navbar navbar-expand-lg my-nav">
+    <div class="container">
+        <a class="navbar-brand text-bold logo" aria-current="page" href="{{ route('home') }}">BestChoice</a>
 
-            <ul class="navbar-nav">
-                @auth
+          {{--  CATEGORIAS DAR FRONT--}}
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categorías
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @foreach ($categories as $category)
+                        <li><a class="dropdown-item" href="{{ route('category.ads', $category) }}">{{ $category->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex justify-content-start">
+            <form class="col-12" role="search">
+                <div class="box-src">
+                    <input type="search" class="form-control form-control-dark text-bg-light"
+                        placeholder="Buscar..." aria-label="Search">
+                </div>
+            </form>
+        </div>
+        {{-- <div class="create-btn">
+            <button><a class="create-btn" href="{{route('ads.create')}}"><i class="fa-solid fa-plus"> Crear anuncio</i></a></button>
+        </div> --}}
+        <button type="button" class="btn btn-outline-warning btn-create">Crear anuncio</button>
+
+
+
+        {{-- FIN CATEGORIAS --}}
+        <ul class="navbar-nav">
+            @auth
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle " id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         Bienvenido {{ Auth::user()->name }}
                     </a>
@@ -15,49 +46,45 @@
                             <hr class="dropdown-divider">
                         </li>
                         @if (Auth::user()->is_admin)
-                        <li><a href="{{route('admin.dashboard')}}" class="dropdown-item">Panel del Administrador</a>
-                        </li>
-
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
+                            <li><a href="{{ route('admin.dashboard') }}" class="dropdown-item">Panel del Administrador</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                         @endif
                         @if (Auth::user()->is_revisor)
-                        <li><a href="{{route('revisor.dashboard')}}" class="dropdown-item">Panel del Revisor</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
+                            <li><a href="{{ route('revisor.dashboard') }}" class="dropdown-item">Panel del Revisor</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                         @endif
                         @if (Auth::user()->is_writer)
-                        <li><a href="{{route('writer.dashboard')}}" class="dropdown-item">Panel del Redactor</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
+                            <li><a href="{{ route('writer.dashboard') }}" class="dropdown-item">Panel del Redactor</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                         @endif
 
                         <li><a href="#" class="dropdown-item"
-                                onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Cerrar sesión</a>
+                                onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Cerrar
+                                sesión</a>
                         </li>
                         <form action="{{ route('logout') }}" method="post" id="form-logout" class="d-none">
                             @csrf
                         </form>
                     </ul>
                 </li>
-                @endauth
-                @guest
-                <li class="nav-menu dropdown">
-                    <a href="#" class="nav-link dropdown-toggle px-3 text-white" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">Bienvenido</a>
 
-                    <ul class="dropdown-menu position-absolute dropfalse" aria-labelledby="navbarDropdown">
-                        <li><a href="{{ route('register') }}" class="dropdown-item">Registrarse</a></li>
-                        <li><a href="{{ route('login') }}" class="dropdown-item">Iniciar sesión</a></li>
+            @endauth
+            @guest
+
+                    <ul class="nav col-12 col-lg-auto p-2 ml-2 justify-content-center">
+
+                        <li><a href="{{ route('login') }}" class="nav-link text-dark">Iniciar sesión</a></li>
+                        <li><a href="{{ route('register') }}" class="nav-link text-dark">Registrarse</a></li>
                     </ul>
-                </li>
-
-                @endguest
-            </ul>
-        </div>
+            @endguest
+        </ul>
+    </div>
     </div>
 </nav>
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,50 @@
 <x-layout>
     <x-slot name='title'>BestChoice - ads</x-slot>
-<div class="container-fluid my-hero">
-    <div class="col-12 mx-5 px-5">
-        <div class="col-md-5 p-lg-5 mx-auto mb-5">
-
-            <div class="container-fluid">
-                <h1 class="display-4 fw-normal my-title">¡Best Choice!</h1>
-            </div>
-            <p class="lead fw-normal my-subtitle">Vende lo que no utilizas en Best Choice</p>
-            <a class="btn btn-create px-2 my-5" href="{{route('ads.create')}}">Crear anuncio</a>
+    <div class="px-4 py-5 my-5 text-center my-hero">
+        <h1 class="display-5 fw-bold">Centered hero</h1>
+        <div class="col-lg-4 mx-auto">
+            <p class="lead mb-4 my-subtitle">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
+                most.</p>
         </div>
-        <div class="product-device shadow-sm d-none d-md-block"></div>
-        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
-</div>
     <div class="container">
         <div class="row">
             @forelse($ads as $ad)
-            <div class="col-md-3 col-sm-6">
-                <div class="product-grid">
-                    <div class="product-image">
-                        <a href="{{ route("ads.show", $ad) }}" class="image">
-                            <img class="pic-1" src="https://i.ebayimg.com/images/g/WuAAAOSw1iFhqrJZ/s-l1600.jpg">
-                        </a>
-                        <div class="price">{{$ad->price}}€</div>
-                        <a href="{{ route("ads.show", $ad) }}" class="add-to-cart">Ver</a>
-                    </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-grid">
+                        <div class="product-image">
+                            <a href="{{ route('ads.show', $ad) }}" class="image">
+                                <img class="pic-1" src="https://i.ebayimg.com/images/g/WuAAAOSw1iFhqrJZ/s-l1600.jpg">
+                            </a>
+                            <div class="price">{{ $ad->price }}€</div>
+                            <a href="{{ route('ads.show', $ad) }}" class="add-to-cart">Ver</a>
+                        </div>
 
-                    <div class="product-content">
-                        <div class="card-body">
-                            <h3 class="card-title">{{$ad->title}}</h3>
-                            <p class="card-text">{{$ad->body}}</p>
-                            <div class="card-subtitle mb-2">
-                                <small>Vendedor: {{$ad->user->name}}</small>
-                                <ul class="rating">
-                                    <li class="fa fa-star"></li>
-                                    <li class="fa fa-star"></li>
-                                    <li class="fa fa-star"></li>
-                                    <li class="fa fa-star"></li>
-                                    <li class="fa fa-star-o"></li>
-                                </ul>
+                        <div class="product-content">
+                            <div class="card-body">
+                                <h3 class="card-title">{{ $ad->title }}</h3>
+                                <p class="card-text">{{ $ad->body }}</p>
+                                <div class="card-subtitle mb-2">
+                                    <small>Vendedor: {{ $ad->user->name }}</small>
+                                    <ul class="rating">
+                                        <li class="fa fa-star"></li>
+                                        <li class="fa fa-star"></li>
+                                        <li class="fa fa-star"></li>
+                                        <li class="fa fa-star"></li>
+                                        <li class="fa fa-star-o"></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
             @empty
-            <div class="col-12">
-                <h2>Uyy...parece que no hay nada de esta categoría</h2>
-                <a href="{{route('ads.create')}}" class="btn btn-success">Vende tu primer objeto</a>
-                <a href="{{route('home')}}" class="btn btn-primary">Vuelve a la home</a>
-            </div>
+                <div class="col-12">
+                    <h2>Uyy...parece que no hay nada de esta categoría</h2>
+                    <a href="{{ route('ads.create') }}" class="btn btn-success">Vende tu primer objeto</a>
+                    <a href="{{ route('home') }}" class="btn btn-primary">Vuelve a la home</a>
+                </div>
             @endforelse
         </div>
 
