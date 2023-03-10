c376127c2164cab97525e3f720444b664daf6811
Leudys Torres <leudystorres30@gmail.com>
2023-03-11 00:04:22 +0100

    traducion show y about

--- resources/views/components/locale.blade.php
+++ resources/views/components/locale.blade.php
@@ -5,7 +5,7 @@
 
 <form action="{{ route('locale.set', $lang) }}" method="POST">
     @csrf
-    <button type="submit" class="nav-link bg-light nav-active" style="backhround: transparent; border:none">
+    <button type="submit" class="nav-link nav-active" style="background: transparent; border:none">
         <span class="flag-icon flag-icon-{{ $country }}"></span>
     </button>
 </form>
