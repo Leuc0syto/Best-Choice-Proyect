9f2dee5dbc877bcab175a0bb28d9dbbcdb843c27
Leudys Torres <leudystorres30@gmail.com>
2023-03-13 18:25:06 +0100

    login

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+let showHide = document.getElementById('show-hide');
+let passwordInput = document.getElementById('password-input');
+
+showHide.addEventListener('click', function() {
+    showHide.classList.toggle('show');
+
+    if(showHide.classList.contains('show')){
+        showHide.classList.remove('fa-eye-slash');
+        showHide.classList.add('fa-eye');
+        passwordInput.classList.setAttribute('type', 'text')
+    }
+    else{
+        showHide.classList.classList('fa-eye-slash');
+        showHide.classList.remove('fa-eye');
+        passwordInput.setAttribute('type', 'password');
+    }
+});
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot name="title">Best Choice - Login</x-slot>
 
-    <div class="container-fluid">
+{{--     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-6 offset-md-3">
                 <h2 class="form-title space-around">{{__('Iniciar sesión')}}</h2>
@@ -37,5 +37,46 @@
                         href="{{ route('register') }}">{{__('¡Registrate!')}}</a></p>
             </div>
         </div>
+    </div> --}}
+
+
+
+    {{--Login --}}
+    @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+    <div class="form-container">
+        <form action="/login" method="POST" role="form" class="form-login">
+            @csrf
+            <h2 class="form-title space-around d-flex justify-content-center">{{__('Iniciar sesión')}}</h2>
+            <label class="lable-login" for="username"></label>
+            <input class="input-login" type="email" name="email" id="email" placeholder="{{__('Tu correo')}}"data-rule="minlen:4" data-msg="Please enter at least 4 characters">
+            <div class="validate"></div>
+            <label lable-login for="password"></label>
+            <div class="pass">
+
+                <input input-login type="password" class="pass" name="password" placeholder="{{__('Tu contraseña')}}" id="password">
+                <div class="validate"></div>
+                <i id="show-hide" class="fa-solid fa-eye-slash"></i>
+
+            </div>
+            <button class="btn-login" type="submit">{{__('Entrar')}}</button>
+            <hr>
+            <div class="social-container">
+                <div class="social">
+                    <div><a class="go" href=""><i class="fab fa-google"></i></a></div>
+                    <div><a class="fb" href=""><i class="fab fa-facebook"></i></a></div>
+                </div>
+                <p class="my-3 p-1">{{__('¿Aún no eres de los nuestros?')}} <a class="btn-create d-flex justify-content-center btn-sm ms-2"
+                    href="{{ route('register') }}">{{__('¡Registrate!')}}</a></p>
+            </div>
+        </form>
     </div>
+
 </x-layout>
