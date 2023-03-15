d4bf5716c6d87060e21aa1241d37b9f7e50c2631
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-15 20:20:35 +0100

    arreglar cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
             </button>
         </div>
     </div>
-
+    <!-- Carousel end -->
 
 
     <div class="container my-4">
@@ -53,39 +53,35 @@
                 <div class="product-grid">
                     <div class="product-image">
                         @if ($ad->images()->count() > 0)
-                            <img src="{{ Storage::url($ad->images()->first()->path) }}" class="card-img-top" alt="...">
+                        <a href="{{ route('ads.show', $ad) }}" class="image">
+                            <img class="pic-1" src="{{ Storage::url($ad->images()->first()->path) }}" alt="...">
+                        </a>
                         @else
-                            <img src="https://i.ebayimg.com/images/g/WuAAAOSw1iFhqrJZ/s-l1600.jpg" alt="...">
+                        <img class="pic-1" src="https://i.ebayimg.com/images/g/WuAAAOSw1iFhqrJZ/s-l1600.jpg" alt="...">
                         @endif
-
-                        {{-- <a href="{{ route('ads.show', $ad) }}" class="image">
-                            <img class="pic-1" src="https://i.ebayimg.com/images/g/WuAAAOSw1iFhqrJZ/s-l1600.jpg">
-                        </a> --}}
 
                         <div class="price">{{ $ad->price }}{{__('€')}}</div>
                         <a href="{{ route('ads.show', $ad) }}" class="add-to-cart">{{__('Ver')}}</a>
                     </div>
 
-                        <div class="product-content">
-                            <div class="card-body">
-                                <h3 class="card-title">{{ $ad->title }}</h3>
-                                <p class="card-text">{{ $ad->body }}</p>
-                                <div class="card-subtitle mb-2">
-                                    <small>{{ __('Vendedor:') }} {{ $ad->user->name }}</small>
-                                    <ul class="rating">
-                                        <li class="fa fa-star"></li>
-                                        <li class="fa fa-star"></li>
-                                        <li class="fa fa-star"></li>
-                                        <li class="fa fa-star"></li>
-                                        <li class="fa fa-star-o"></li>
-                                    </ul>
-                                </div>
+                    <div class="product-content">
+                        <div class="card-body">
+                            <h3 class="card-title">{{ $ad->title }}</h3>
+                            <p class="card-text">{{ $ad->body }}</p>
+                            <div class="card-subtitle mb-2">
+                                <small>{{ __('Vendedor:') }} {{ $ad->user->name }}</small>
+                                <ul class="rating">
+                                    <li class="fa fa-star"></li>
+                                    <li class="fa fa-star"></li>
+                                    <li class="fa fa-star"></li>
+                                    <li class="fa fa-star"></li>
+                                    <li class="fa fa-star-o"></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
             @empty
             <div class="col-12">
                 <h2>{{__('Uyy...parece que no hay nada de esta categoría')}}</h2>
