d700d8a38b582a203280a40fd84a0af84d1f843c
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-21 19:51:19 +0100

    front de modal

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,74 +5,78 @@
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            @auth
-                
-                    <button type="button"
-                        class="btn btn-warning btn-block my-btn-call nav col-lg-auto p-2 ml-2 justify-content-center">
-                        <a href="{{ route('ads.create') }}"
-                            class="text-decoration-none text-dark">{{ __('Crear anuncio') }}</a>
-                    </button>
-                
-            @endauth
-        </ul>
+                @auth
+
+                {{-- <button type="button"
+                    class="btn btn-warning btn-block my-btn-call nav col-lg-auto p-2 ml-2 justify-content-center"
+                    data-bs-toggle="modal" data-bs-target="#staticBackdrop">{{ __('Crear anuncio') }}</a>
+                </button> --}}
+
+                <button type="button"
+                class="btn btn-warning btn-block my-btn-call nav col-lg-auto p-2 ml-2 justify-content-center">
+                <a href="{{ route('ads.create') }}"
+                    class="text-decoration-none text-dark">{{ __('Crear anuncio') }}</a>
+                </button>
+
+                @endauth
+            </ul>
 
 
 
             <ul class="navbar-nav me-2">
                 @auth
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle " id="navbarDropdown" role="button" href="#"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @if (Auth::user()->is_revisor)
-                                <li>
-                                    <a class="dropdown-item" href="{{ route('revisor.home') }}">
-                                        {{ __('Anuncios sin revisar') }}
-                                        <span class="badge rounded-pill bg-danger">
-                                            {{ \App\Models\Ad::ToBeRevisionedCount() }}
-                                        </span>
-                                    </a>
-                                </li>
-                            @endif
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle " id="navbarDropdown" role="button" href="#"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @if (Auth::user()->is_revisor)
+                        <li>
+                            <a class="dropdown-item" href="{{ route('revisor.home') }}">
+                                {{ __('Anuncios sin revisar') }}
+                                <span class="badge rounded-pill bg-danger">
+                                    {{ \App\Models\Ad::ToBeRevisionedCount() }}
+                                </span>
+                            </a>
+                        </li>
+                        @endif
 
-                            <form id="logoutForm" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <a id="logoutBtn" class="dropdown-item" href="#">{{ __('Cerrar sesión') }}</a>
-                            </form>
-                        </ul>
-                    </li>
+                        <form id="logoutForm" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <a id="logoutBtn" class="dropdown-item" href="#">{{ __('Cerrar sesión') }}</a>
+                        </form>
+                    </ul>
+                </li>
 
                 @endauth
                 @guest
-                    <ul class="navbar-nav">
+                <ul class="navbar-nav">
 
-                        {{-- <li><a href="{{ route('login') }}" class="nav-link text-dark">{{ __('Iniciar sesión') }}</a></li> --}}
-                        <a class="nav-link text-dark" data-bs-toggle="modal" href="#exampleModalToggle"
-                            role="button">{{ __('Iniciar sesión') }}</a>
-                        {{-- <li><a href="{{ route('register') }}" class="nav-link text-dark">{{ __('Registrar') }}</a></li> --}}
-                    </ul>
+                    <a class="nav-link text-dark" data-bs-toggle="modal" href="#exampleModalToggle"
+                        role="button">{{ __('Iniciar sesión') }}</a>
+                </ul>
                 @endguest
             </ul>
 
             <ul class="navbar-nav col-12 col-lg-auto p-2 ml-2">
                 <li class="nav-item dropdown">
-                    <a class="nav-link text-dark dropdown-toggle" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         <span
                             class="flag-icon flag-icon-{{ Config::get('languages')[App::getLocale()]['flag-icon'] }}"></span>
                         {{ Config::get('languages')[App::getLocale()]['display'] }}
                     </a>
                     <div class="dropdown-menu my-dropdown-width" aria-labelledby="navbarDropdownMenuLink">
                         @foreach (Config::get('languages') as $lang => $language)
-                            @if ($lang != App::getLocale())
-                                <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span
-                                        class="flag-icon flag-icon-{{ $language['flag-icon'] }}"></span>
-                                    {{ $language['display'] }}</a>
-                            @endif
+                        @if ($lang != App::getLocale())
+                        <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span
+                                class="flag-icon flag-icon-{{ $language['flag-icon'] }}"></span>
+                            {{ $language['display'] }}</a>
+                        @endif
                         @endforeach
                     </div>
                 </li>
@@ -83,33 +87,31 @@
 
 {{-- Modal --}}
 
-
+{{-- Login Modal --}}
 <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
     tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content form-login">
-            <div class="modal-header d-flex justify-content-center" style="background-color:#d3a1a0;">
+            <div class="modal-header d-flex justify-content-center">
                 <h5 class="modal-title animate__heartBeat animate__delay-3s" id="exampleModalToggleLabel">
                     {{ __('Iniciar sesión') }}</h5>
             </div>
             <div class="modal-body">
                 @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
                 @endif
                 <div class="form-container">
                     <form action="/login" method="POST" role="form">
                         @csrf
-                        {{-- <h2 class="animate__heartBeat animate__delay-3s form-title d-flex justify-content-center">
-                        {{__('Iniciar sesión')}}</h2> --}}
-                        <label class="lable-login" for="username"></label>
-                        <input class="input-login form-control forms_field-input border-0 " type="email"
-                            name="email" id="email" placeholder="{{ __('Tu correo') }}" data-rule="minlen:4"
+                        {{-- <label for="email" class="form-label lable-login ">{{ __('Correo:') }} </label> --}}
+                        <input class="input-login form-control forms_field-input border-0 " type="email" name="email"
+                            id="email" placeholder="{{ __('Tu correo') }}" data-rule="minlen:4"
                             data-msg="Please enter at least 4 characters">
                         <div class="validate"></div>
                         <label lable-login for="password"></label>
@@ -120,8 +122,7 @@
                             <i id="show-hide" class="fa-solid fa-eye-slash"></i>
                         </div>
                         <button type="submit"
-                            class="btn btn-login my-btn-call col-12 py-3 mt-5 text-dark justify-content-center"
-                            style="background-color:#d3a1a0;">{{ __('Entrar') }}
+                            class="btn btn-login btn-warning my-btn-call col-12 py-3 mt-5 text-dark justify-content-center">{{ __('Entrar') }}
                         </button>
                         <hr>
                         <div class="social-container">
@@ -133,40 +134,37 @@
                     </form>
                 </div>
             </div>
-            <div class="modal-footer" style="background-color:#d3a1a0;">
-                <p class="my-3 p-2 p-login">{{ __('¿Aún no eres de los nuestros?') }} <a
-                        class="my-btn btn btn-outline-warning a-login btn-sm ms-2"
-                        href="{{ route('register') }}"data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
+            <div class="modal-footer">
+                <p class="p-login">{{ __('¿Aún no eres de los nuestros?') }} <a
+                        class="my-btn btn btn-outline-warning a-login btn-sm ms-2" href="{{ route('register') }}"
+                        data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
                         data-bs-dismiss="modal">{{ __('¡Registrate!') }}</a>
                 </p>
-                {{-- <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button> --}}
             </div>
         </div>
     </div>
-</div>
+</div> 
+{{-- Create Account Modal --}}
 <div class="modal fade " id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
     tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content form-register">
-            <div class="modal-header d-flex justify-content-center" style="background-color:#d3a1a0;">
+            <div class="modal-header d-flex justify-content-center">
                 <h5 class="modal-title animate__heartBeat animate__delay-3s" id="exampleModalToggleLabel2">
                     {{ __('Crear cuenta') }}</h5>
-
             </div>
             <div class="modal-body label-login">
                 @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
                 @endif
                 <div>
                     <form action="/register" method="POST" role="form">
-                        {{-- <h2 class="animate__heartBeat animate__delay-3s form-title d-flex justify-content-center mb-4">
-                        {{__('Crear cuenta')}}</h2> --}}
                         @csrf
 
                         {{-- Nombre --}}
@@ -176,6 +174,7 @@
                             placeholder="{{ __('Tu nombre') }}" data-rule="minlen:4"
                             data-msg="Please enter at least 4 characters">
                         <div class="validate"></div>
+
 
                         {{-- Correo --}}
 
@@ -207,7 +206,7 @@
 
 
                         <button type="submit"
-                            class="btn btn-login my-btn-call col-12 py-3 mt-4 text-dark justify-content-center" style="background-color:#d3a1a0;">
+                            class="btn btn-login my-btn-call col-12 py-3 mt-4 text-dark justify-content-center">
                             {{ __('Crear cuenta') }}
                         </button>
                         <div class="social-container">
@@ -216,7 +215,7 @@
                     </form>
                 </div>
             </div>
-            <div class="modal-footer" style="background-color:#d3a1a0;">
+            <div class="modal-footer">
                 <p class="my-3 p-2 mb-5 p-login">{{ __('¿Ya eres de los nuestros?') }} <a
                         class="my-btn btn btn-outline-warning a-login btn-sm ms-2" href="{{ route('login') }}"
                         data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
@@ -227,5 +226,94 @@
         </div>
     </div>
 </div>
+{{-- Create Ad Modal --}}
+{{-- <div wire:ignore.self class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content form-register">
+            <div class="modal-header d-flex justify-content-center">
+                <h5 class="modal-title" id="exampleModalToggleLabel2">
+                    {{ __('Crear anuncio') }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body form-create">
+                @if (session()->has('message'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('message') }}
+                </div>
+                @endif
+
+                <form wire:submit.prevent="store">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label lable-login">{{ __('Título:') }} </label>
+                        <input wire:model="title" type="text"
+                            class="form-control input-login @error('title') is-invalid @enderror">
+                        @error('title')
+                        {{ $message }}
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="price" class="form-label lable-login ">{{ __('Precio:') }} </label>
+                        <input wire:model="price" type="number" min="0"
+                            class="form-control input-login @error('price') is-invalid @enderror">
+                        @error('price')
+                        {{ $message }}
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="body" class="form-label lable-login">{{ __('Descripción:') }} </label>
+                        <textarea wire:model="body" cols="20" rows="5"
+                            class=" input-login form-control  @error('body') is-invalid @enderror"></textarea>
+                        @error('body')
+                        {{ $message }}
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <select wire:model.defer="category" class="form-control input-login">
+                            <option value="">{{ __('Seleccionar Categoría') }}</option>
+                            @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ __($category->name) }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <input wire:model="temporary_images" type="file" name="images" multiple
+                            class="form-control in input-login @error('temporary_images.*') is-invalid @enderror">
+                        @error('temporary_images.*')
+                        <p class="text-danger mt-2">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    @if (!empty($images))
+                    <div class="row">
+                        <div class="col-12">
+                            <p>{{ __('Vista previa') }}:</p>
+                            <div class="row">
+                                @foreach ($images as $key => $image)
+                                <div class="col-12 col-md-4">
+                                    <img src="{{ $image->temporaryUrl() }}" alt="" class="img-fluid">
+                                    <button type="button" class="btn btn-danger"
+                                        wire:click="removeImage({{ $key }})">{{ __('Eliminar') }}</button>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                    <div class="my-3 d-flex justify-content-center">
+                        <button type="submit"
+                            class="btn btn-login btn-warning my-btn-call text-black col-12 py-3 justify-content-center">{{ __('Crear') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> --}}
 
 {{-- End Modal --}}
