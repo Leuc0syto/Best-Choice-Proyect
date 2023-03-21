241e120e22c7c7a478079aaf8bc1e41716097437
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-21 20:57:23 +0100

    Nueva paginacion y front login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,9 +16,9 @@
                 </button> --}}
 
                 <button type="button"
-                class="btn btn-warning btn-block my-btn-call nav col-lg-auto p-2 ml-2 justify-content-center">
-                <a href="{{ route('ads.create') }}"
-                    class="text-decoration-none text-dark">{{ __('Crear anuncio') }}</a>
+                    class="btn btn-warning btn-block my-btn-call nav col-lg-auto p-2 ml-2 justify-content-center">
+                    <a href="{{ route('ads.create') }}"
+                        class="text-decoration-none text-dark">{{ __('Crear anuncio') }}</a>
                 </button>
 
                 @endauth
@@ -110,7 +110,7 @@
                     <form action="/login" method="POST" role="form">
                         @csrf
                         {{-- <label for="email" class="form-label lable-login ">{{ __('Correo:') }} </label> --}}
-                        <input class="input-login form-control forms_field-input border-0 " type="email" name="email"
+                        <input class="input-login form-control forms_field-input border-0" type="email" name="email"
                             id="email" placeholder="{{ __('Tu correo') }}" data-rule="minlen:4"
                             data-msg="Please enter at least 4 characters">
                         <div class="validate"></div>
@@ -130,20 +130,18 @@
                                 <div><a class="go" href=""><i class="fab fa-google"></i></a></div>
                                 <div><a class="fb" href=""><i class="fab fa-facebook"></i></a></div>
                             </div>
+                            <p class="my-5 px-5 d-flex justify-content-center">{{ __('¿Aún no eres de los nuestros?') }} <a
+                                    class="my-btn btn btn-outline-warning a-login btn-sm ms-2"
+                                    href="{{ route('register') }}" data-bs-target="#exampleModalToggle2"
+                                    data-bs-toggle="modal" data-bs-dismiss="modal">{{ __('¡Registrate!') }}</a>
+                            </p>
                         </div>
                     </form>
                 </div>
             </div>
-            <div class="modal-footer">
-                <p class="p-login">{{ __('¿Aún no eres de los nuestros?') }} <a
-                        class="my-btn btn btn-outline-warning a-login btn-sm ms-2" href="{{ route('register') }}"
-                        data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
-                        data-bs-dismiss="modal">{{ __('¡Registrate!') }}</a>
-                </p>
-            </div>
         </div>
     </div>
-</div> 
+</div>
 {{-- Create Account Modal --}}
 <div class="modal fade " id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
     tabindex="-1">
@@ -210,18 +208,14 @@
                             {{ __('Crear cuenta') }}
                         </button>
                         <div class="social-container">
+                            <p class="my-5 px-5 d-flex justify-content-center">{{ __('¿Ya eres de los nuestros?') }} <a
+                                class="my-btn btn btn-outline-warning a-login btn-sm ms-2" href="{{ route('login') }}"
+                                data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
+                                data-bs-dismiss="modal">{{ __('Identifícate!') }}</a>
+                        </p>
                         </div>
-
                     </form>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <p class="my-3 p-2 mb-5 p-login">{{ __('¿Ya eres de los nuestros?') }} <a
-                        class="my-btn btn btn-outline-warning a-login btn-sm ms-2" href="{{ route('login') }}"
-                        data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
-                        data-bs-dismiss="modal">{{ __('Identifícate!') }}</a>
-                </p>
-                {{-- <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button> --}}
             </div>
         </div>
     </div>
@@ -234,86 +228,85 @@
             <div class="modal-header d-flex justify-content-center">
                 <h5 class="modal-title" id="exampleModalToggleLabel2">
                     {{ __('Crear anuncio') }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
 
-            <div class="modal-body form-create">
-                @if (session()->has('message'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('message') }}
+<div class="modal-body form-create">
+    @if (session()->has('message'))
+    <div class="alert alert-success" role="alert">
+        {{ session('message') }}
+    </div>
+    @endif
+
+    <form wire:submit.prevent="store">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label lable-login">{{ __('Título:') }} </label>
+            <input wire:model="title" type="text" class="form-control input-login @error('title') is-invalid @enderror">
+            @error('title')
+            {{ $message }}
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="price" class="form-label lable-login ">{{ __('Precio:') }} </label>
+            <input wire:model="price" type="number" min="0"
+                class="form-control input-login @error('price') is-invalid @enderror">
+            @error('price')
+            {{ $message }}
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="body" class="form-label lable-login">{{ __('Descripción:') }} </label>
+            <textarea wire:model="body" cols="20" rows="5"
+                class=" input-login form-control  @error('body') is-invalid @enderror"></textarea>
+            @error('body')
+            {{ $message }}
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <select wire:model.defer="category" class="form-control input-login">
+                <option value="">{{ __('Seleccionar Categoría') }}</option>
+                @foreach ($categories as $category)
+                <option value="{{ $category->id }}">{{ __($category->name) }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-3">
+            <input wire:model="temporary_images" type="file" name="images" multiple
+                class="form-control in input-login @error('temporary_images.*') is-invalid @enderror">
+            @error('temporary_images.*')
+            <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        @if (!empty($images))
+        <div class="row">
+            <div class="col-12">
+                <p>{{ __('Vista previa') }}:</p>
+                <div class="row">
+                    @foreach ($images as $key => $image)
+                    <div class="col-12 col-md-4">
+                        <img src="{{ $image->temporaryUrl() }}" alt="" class="img-fluid">
+                        <button type="button" class="btn btn-danger"
+                            wire:click="removeImage({{ $key }})">{{ __('Eliminar') }}</button>
+                    </div>
+                    @endforeach
                 </div>
-                @endif
-
-                <form wire:submit.prevent="store">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="title" class="form-label lable-login">{{ __('Título:') }} </label>
-                        <input wire:model="title" type="text"
-                            class="form-control input-login @error('title') is-invalid @enderror">
-                        @error('title')
-                        {{ $message }}
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="price" class="form-label lable-login ">{{ __('Precio:') }} </label>
-                        <input wire:model="price" type="number" min="0"
-                            class="form-control input-login @error('price') is-invalid @enderror">
-                        @error('price')
-                        {{ $message }}
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="body" class="form-label lable-login">{{ __('Descripción:') }} </label>
-                        <textarea wire:model="body" cols="20" rows="5"
-                            class=" input-login form-control  @error('body') is-invalid @enderror"></textarea>
-                        @error('body')
-                        {{ $message }}
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
-                        <select wire:model.defer="category" class="form-control input-login">
-                            <option value="">{{ __('Seleccionar Categoría') }}</option>
-                            @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ __($category->name) }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="mb-3">
-                        <input wire:model="temporary_images" type="file" name="images" multiple
-                            class="form-control in input-login @error('temporary_images.*') is-invalid @enderror">
-                        @error('temporary_images.*')
-                        <p class="text-danger mt-2">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    @if (!empty($images))
-                    <div class="row">
-                        <div class="col-12">
-                            <p>{{ __('Vista previa') }}:</p>
-                            <div class="row">
-                                @foreach ($images as $key => $image)
-                                <div class="col-12 col-md-4">
-                                    <img src="{{ $image->temporaryUrl() }}" alt="" class="img-fluid">
-                                    <button type="button" class="btn btn-danger"
-                                        wire:click="removeImage({{ $key }})">{{ __('Eliminar') }}</button>
-                                </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                    @endif
-                    <div class="my-3 d-flex justify-content-center">
-                        <button type="submit"
-                            class="btn btn-login btn-warning my-btn-call text-black col-12 py-3 justify-content-center">{{ __('Crear') }}</button>
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
+        @endif
+        <div class="my-3 d-flex justify-content-center">
+            <button type="submit"
+                class="btn btn-login btn-warning my-btn-call text-black col-12 py-3 justify-content-center">{{ __('Crear') }}</button>
+        </div>
+    </form>
+</div>
+</div>
+</div>
 </div> --}}
 
 {{-- End Modal --}}
