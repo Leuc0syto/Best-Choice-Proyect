caaf46a52a99777185b0c8bf379a76d62dbc8017
Micky <micpro79@mail.ru>
2023-03-21 19:44:56 +0100

    user7

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearchImage;
 use App\Models\Ad;
 use Livewire\Component;
@@ -79,6 +80,7 @@ class CreateAd extends Component
                 ]);
                 dispatch(new ResizeImage($newImage->path, 400, 400));
                 dispatch(new GoogleVisionSafeSearchImage($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -14,12 +14,41 @@
                             <div class="col-md-3">
                                 <b>{{__('Imágenes') }}</b>
                             </div>
-                            <div class="col-9">
+                            <div class="col-md-9">
                                 <div class="row">
                                     @forelse ($ad->images as $image)
                                     <div class="col-md-4">
                                         {{-- <img src="{{ $image->getUrl() }}" class="img-fluid" alt="..."> --}}
-                                        <img src="{{ !$ad->images()->get()->isEmpty() ? $ad->images()->first()->getUrl(400,400) : 'https://via.placeholder.com/150'}}" class="card-img-top" alt="...">
+                                        <img src="{{ $image->getUrl(400, 400) ?? 'https://via.placeholder.com/150'}}"
+                                            class="card-img-top img-fluid" alt="...">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <b>Safe Search</b><br>
+                                        <b>Adult :</b> <i class="bi bi-circle-fill {{ $image->adult }}"></i>
+                                        [{{ $image->adult }}] <br>
+
+                                        <b>Spoof :</b> <i class="bi bi-circle-fill {{ $image->spoof }}"></i>
+                                        [{{ $image->spoof }}] <br>
+
+                                        <b>Medical :</b> <i class="bi bi-circle-fill {{ $image->medical }}"></i>
+                                        [{{ $image->medical }}] <br>
+
+                                        <b>Violence :</b> <i class="bi bi-circle-fill {{ $image->violence }}"></i>
+                                        [{{ $image->violence }}] <br>
+
+                                        <b>Racy :</b> <i class="bi bi-circle-fill {{ $image->racy }}"></i> [{{ $image->racy }}]
+                                        <br><br>
+
+                                        <b>Labels</b><br>
+                                        @forelse ($image->getLabels() as $label)
+                                            <a href="#" class="btn btn-info btn-sm m-1">{{ $label }}</a>
+                                        @empty
+                                        No labels
+                                        @endforelse
+                                        <br><br>
+                                        
+                                        <b>Id:</b>{{ $image->id }} <br>
+                                        <b>Uri:</b>{{ Storage::url($image->path) }} <br>
                                     </div>
                                     @empty
                                     <div class="col-12">
