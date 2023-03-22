a9873565559246d607c0ebe450a3a548fb6e10ce
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-22 21:51:53 +0100

    categorias api

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -64,20 +64,19 @@
                                             class="card-img-top img-fluid" alt="...">
                                     </div>
                                     <div class="col-md-8">
-                                        <p class="mb-2"><b>{{__('Adultos')}}: </b><i class="bi bi-circle-fill {{ $image->adult }}"></i>
-                                        [{{ $image->adult }}]<br></p>
+                                        <p class="mb-2"><b>{{__('Contenido pornográfico')}}: </b><i class="bi bi-circle-fill {{ $image->adult }}"></i>
+                                        <br></p>
+
+                                        <p class="mb-2"><b>{{__('Erótico')}}: </b><i class="bi bi-circle-fill {{ $image->racy }}"></i>
+                                            <br></p>
 
                                         <p class="mb-2"><b>{{__('Degradante')}}: </b><i class="bi bi-circle-fill {{ $image->spoof }}"></i>
-                                        [{{ $image->spoof }}] <br></p>
+                                        <br></p>
 
                                         <p class="mb-2"><b>{{__('Médico')}}: </b><i class="bi bi-circle-fill {{ $image->medical }}"></i>
-                                        [{{ $image->medical }}] <br></p>
+                                        <br></p>
 
                                         <p class="mb-2"><b>{{__('Violencia')}}: </b><i class="bi bi-circle-fill {{ $image->violence }}"></i>
-                                        [{{ $image->violence }}] <br></p>
-
-                                        <p class="mb-2"><b>{{__('Racial')}}: </b><i class="bi bi-circle-fill {{ $image->racy }}"></i>
-                                        [{{ $image->racy }}]
                                         <br></p>
 
 
