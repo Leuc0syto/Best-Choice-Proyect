323573b304f88094e524efd734896fdb3f54c8c1
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-24 18:39:36 +0100

    carrousel con enlaces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,35 +15,45 @@
             <!-- The slideshow/carousel -->
             <div class="carousel-inner">
                 <div class="carousel-item item active">
-                    <a href=""><img src="{{ asset('assets/img/hero-new.jpg') }}" alt="Novedades"
-                            class="d-flex w-100 rounded" style=""></a>
+                    <a href="#Novedades"><img src="{{ asset('assets/img/hero-new.jpg') }}" alt="Novedades"
+                            class="d-flex w-100 rounded"></a>
                     <div class="carousel-caption d-none d-md-block">
                         <h3 class="text-uppercase">{{ __('Novedades') }}</h3>
                     </div>
                 </div>
                 <div class="carousel-item item">
-                    <a href=""><img src="{{ asset('assets/img/hero-men.jpg') }}" alt="Moda hombre"
-                            class="d-flex w-100 rounded"></a>
-                    <div class="carousel-caption d-none d-md-block col-md-12 text-right text-dark">
-                        <h3 class="text-uppercase">{{ __('Ropa hombre') }}</h3>
-                    </div>
+                    @foreach ($categories as $category)
+                    <a href="{{ route('category.ads', $category) }}">
+                        @switch( __($category->name))
+                        @case( __('Hombre'))
+                        <img src="{{ asset('assets/img/hero-men.jpg') }}" alt="Moda hombre"
+                            class="d-flex w-100 rounded">
+                        <div class="carousel-caption d-none d-md-block col-md-12 text-right text-dark">
+                            <h3 class="text-uppercase">{{ __('Ropa hombre') }}</h3>
+                        </div>
                 </div>
+                @break
+                @case( __('Mujer'))
                 <div class="carousel-item item">
-                    <a href=""><img src="{{ asset('assets/img/hero-women.jpg') }}" alt="Moda mujer"
-                            class="d-flex w-100 rounded"></a>
+                    <img src="{{ asset('assets/img/hero-women.jpg') }}" alt="Moda mujer" class="d-flex w-100 rounded">
                     <div class="carousel-caption d-none d-md-block col-md-3 text-left text-dark">
                         <h3 class="text-uppercase">{{ __('Ropa mujer') }}</h3>
                     </div>
                 </div>
+                @break
+                @default
+                @endswitch
+                </a>
+                @endforeach
             </div>
 
             <!-- Left and right controls/icons -->
-            {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev"> --}}
-            {{-- <span class="carousel-control-prev-icon"></span> --}}
+            {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-                {{-- <span class="carousel-control-next-icon"></span> --}}
-            </button>
+                <span class="carousel-control-next-icon"></span>
+            </button> --}}
         </div>
     </div>
     <!-- Carousel end -->
@@ -125,46 +135,22 @@
         </ul>
     </div>
 
-    {{-- <div class="categories">
-    <div class="category">
-        <a href="#"><img src="{{asset('assets/img/enfermero.png')}}" alt="Hombre"></a>
-    <p>{{__('Hombre')}}</p>
-    </div>
-    <div class="category">
-        <a href="#"><img src="{{asset('assets/img/mujer.png')}}" alt="Mujer"></a>
-        <p>{{__('Mujer')}}</p>
-    </div>
-    <div class="category">
-        <a href="#"><img src="{{asset('assets/img/nino.png')}}" alt="Niños"></a>
-        <p>{{__('Niños')}}</p>
-    </div>
-    <div class="category">
-        <a href="#"><img src="{{asset('assets/img/joyas.png')}}" alt="Accesorios"></a>
-        <p>{{__('Accesorios')}}</p>
-    </div>
-    <div class="category">
-        <a href="#"><img src="{{asset('assets/img/zapato-inteligente.png')}}" alt="Calzado"></a>
-        <p>{{__('Calzado')}}</p>
-    </div>
-    </div> --}}
-
-    </div>
-
-    </div>
     <!-- Finder and categories end -->
 
     <!-- Card container -->
     <div class="container mt-5">
-        <h3 class="text-center">{{ __('Últimos anuncios') }}</h3>
+        <h3 class="text-center" id="Novedades">{{ __('Últimos anuncios') }}</h3>
         <div class="row">
             @forelse($ads as $ad)
             <div class="col-md-3 col-sm-6 col-6 my-2">
                 <div class="product-grid">
                     <div class="product-image">
-                        <a href="{{ route('ads.show', $ad) }}">
+                        <a href="{{ route('ads.show', $ad) }}" class="add-to-cart">
                             <img src="{{ !$ad->images()->get()->isEmpty() ? $ad->images()->first()->getUrl(400,400) : 'https://via.placeholder.com/150'}}"
                                 class="card-img-top my-card" alt="...">
                             <div class="price">{{ $ad->price }}{{ __('€') }}</div>
+
+
                         </a>
                     </div>
                     <div class="product-content">
