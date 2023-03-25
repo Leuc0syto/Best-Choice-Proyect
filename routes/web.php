3c0425b3f4c5e7ae271047b49e849f145faeeb37
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-03-25 18:13:32 +0100

    cambios admin en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\RevisorController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use League\CommonMark\Extension\Footnote\Node\FootnoteRef;
 
 /*
@@ -18,17 +19,20 @@ use League\CommonMark\Extension\Footnote\Node\FootnoteRef;
 |
 */
 
-
-
 Route::get('/', [PublicController::class, 'index'])->name('home');
 
+Route::get('/about', [PublicController::class, 'about'])->name('about');
+Route::get('/privacy', [PublicController::class, 'privacy'])->name('privacy');
+Route::get('/conditions', [PublicController::class, 'conditions'])->name('conditions');
+Route::get('/category/{category:name}/ads', [PublicController::class, 'adsByCategory'])->name('category.ads');
+Route::get('/user/{user:name}/ads', [AdController::class, 'adsByUser'])->name('user.ads');
 Route::get("/search", [PublicController::class, 'search'])->name('search');
 
-Route::get('/category/{category:name}/ads', [PublicController::class, 'adsByCategory'])->name('category.ads');
-
-
+// Rutas de anuncios
+Route::get('/destroy/{ad}', [AdController::class, 'destroy'])->name('ad.destroy');
 Route::get('/ads/create', [AdController::class,'create'])->name('ads.create');
 Route::get('/ads/{ad}', [AdController::class,'show'])->name('ads.show');
+Route::post('/locale/{locale}', [PublicController::class, 'setLocale'])->name('locale.set');
 
 Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLang']);
 
@@ -37,27 +41,16 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/revisor/{user}/make',  [RevisorController::class,'makeRevisor'])->name('revisor.make');
 });
 
-Route::middleware(['isAdmin'])->group(function(){
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin.home');
-    Route::patch('/admin/revisor/{revisor}/accept', [AdminController::class, 'acceptRevisor'])->name('admin.revisor.accept');
-    Route::patch('/admin/revisor/{revisor}/reject', [AdminController::class, 'rejectRevisor'])->name('admin.revisor.reject');
-});
-
-
 Route::middleware(['isRevisor'])->group(function(){
     Route::get('/revisor', [RevisorController::class, 'index'])->name('revisor.home');
     Route::patch('/revisor/ad/{ad}/accept', [RevisorController::class, 'acceptAd'])->name('revisor.ad.accept');
     Route::patch('/revisor/ad/{ad}/reject', [RevisorController::class, 'rejectAd'])->name('revisor.ad.reject');
 });
 
-Route::post('/locale/{locale}', [PublicController::class, 'setLocale'])->name('locale.set');
-
-// FOOTER
-Route::get('/about', [PublicController::class, 'about'])->name('about');
-Route::get('/privacy', [PublicController::class, 'privacy'])->name('privacy');
-Route::get('/conditions', [PublicController::class, 'conditions'])->name('conditions');
-
-// Route::delete ('/ad/{ad}', [AdController::class, 'destroy'])->name('ad.destroy');
+Route::middleware(['isAdmin'])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.home');
+    Route::patch('/admin/user/{user}/delete', [AdminController::class, 'deleteRevisor'])->name('deleteRevisor');
+    Route::get('/admin/{user:name}/adsrevised', [AdminController::class, 'adsByRevisor'])->name('adsByRevisor');
+});
 
 
-Route::get('/destroy/{ad}', [AdController::class, 'destroy'])->name('ad.destroy');
