be7f7a876114462c7f1d6cc60aa80521a1e6112f
Micky <micpro79@mail.ru>
2023-03-25 02:01:49 +0100

    delete

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -6,7 +6,6 @@ use App\Models\Ad;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AdController extends Controller
 {
